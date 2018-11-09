7cf3e66268680e6afbd6e7fcedb5962e9f3e8fed
Andrew Woodward <woodygoalie007@gmail.com>
2018-11-09 21:08:40 +0000

    simplified animation with pointer array, added text to screen

--- testing/display/display.ino
+++ testing/display/display.ino
@@ -6,41 +6,18 @@
 
 Adafruit_SSD1306 display = Adafruit_SSD1306();
  
-#if defined(ESP8266)
-  #define BUTTON_A 0
-  #define BUTTON_B 16
-  #define BUTTON_C 2
-  #define LED      0
-#elif defined(ESP32)
-  #define BUTTON_A 15
-  #define BUTTON_B 32
-  #define BUTTON_C 14
-  #define LED      13
-#elif defined(ARDUINO_STM32F2_FEATHER)
-  #define BUTTON_A PA15
-  #define BUTTON_B PC7
-  #define BUTTON_C PC5
-  #define LED PB5
-#elif defined(TEENSYDUINO)
-  #define BUTTON_A 4
-  #define BUTTON_B 3
-  #define BUTTON_C 8
-  #define LED 13
-#elif defined(ARDUINO_FEATHER52)
-  #define BUTTON_A 31
-  #define BUTTON_B 30
-  #define BUTTON_C 27
-  #define LED 17
-#else // 32u4, M0, M4, and 328p
-  #define BUTTON_A 9
-  #define BUTTON_B 6
-  #define BUTTON_C 5
-  #define LED      13
-#endif
+#define BUTTON_A 9
+#define BUTTON_B 6
+#define BUTTON_C 5
+#define LED      13
+
  
 #if (SSD1306_LCDHEIGHT != 32)
  #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
+
+// create a pointer array so that the bitmaps can be called in sequence easily
+const unsigned char* animation[8] = {ullr1, ullr2, ullr3, ullr3, ullr4, ullr5, ullr6, ullr7};
  
 void setup() {  
   Serial.begin(9600);
@@ -68,68 +45,20 @@ void setup() {
   pinMode(BUTTON_C, INPUT_PULLUP);
 
  
-/*  // text display tests
+  // text display setup (not sure if this needs to be called after clearDisplay in loop)
   display.setTextSize(1);
   display.setTextColor(WHITE);
-  display.setCursor(0,0);
-  display.print("Connecting to SSID\n'adafruit':");
-  display.print("connected!");
-  display.println("IP: 10.0.1.23");
-  display.println("Sending val #0");
-  display.setCursor(0,0);
-  display.display(); // actually display all of the above
-  */
 }
  
  
 void loop() {
-  display.clearDisplay();
-  display.drawBitmap(0, 0, ullr1, 51, 29, WHITE);
-  //yield();
-  display.display();
-  
-  delay(500);
-  
-  display.clearDisplay();
-  display.drawBitmap(0, 0, ullr2, 51, 29, WHITE);
-  //yield();
-  display.display();
-  
-  delay(500);
-  
-  display.clearDisplay();
-  display.drawBitmap(0, 0, ullr3, 51, 29, WHITE);
-  //yield();
-  display.display();
-
-  delay(500);
-
-  display.clearDisplay();
-  display.drawBitmap(0, 0, ullr4, 51, 29, WHITE);
-  //yield();
-  display.display();
-
-  delay(500);
-
-  display.clearDisplay();
-  display.drawBitmap(0, 0, ullr5, 51, 29, WHITE);
-  //yield();
-  display.display();
-
-  delay(500);
-
-  display.clearDisplay();
-  display.drawBitmap(0, 0, ullr6, 51, 29, WHITE);
-  //yield();
-  display.display();
-
-  delay(500);
-
-  display.clearDisplay();
-  display.drawBitmap(0, 0, ullr7, 51, 29, WHITE);
-  //yield();
-  display.display();
-
-  delay(1000);
-  
+  for(int i=0; i<8; i++){
+    display.clearDisplay();
+    display.drawBitmap(0, 1, animation[i], 51, 29, WHITE);
+    display.setCursor(60,10);
+    display.print("Testing ");
+    display.print(i);
+    display.display();
+    delay(500); 
+  }
 }
