19da67b6116339fddb61ed8dad43f32ab3645f81
Andrew Woodward <woodygoalie007@gmail.com>
2018-11-14 22:39:08 +0000

    added sd logging to stable, files for each trick type are created and IMU data is appended

--- src/stable/stable.ino
+++ src/stable/stable.ino
@@ -1,5 +1,6 @@
 #include <SPI.h>
 #include <Wire.h>
+#include <SD.h>
 #include <string.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
@@ -21,11 +22,14 @@ MPU9250 IMU(Wire,0x68);
  #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
+// for SD card communication
+const int chipSelect = 4; // this is the default on the adafruit feather datalogger
+
 // create a pointer array so that the bitmaps can be called in sequence easily
 const unsigned char* animation[7] = {ullr1, ullr2, ullr3, ullr4, ullr5, ullr6, ullr7};
 const unsigned char* animation2[12] = {jump1, jump2, jump3, jump4, jump5, jump6, jump7, jump8, jump9, jump10, jump11, jump12};
 
-const char *dataLabelString[5] = {"Jump","360", "540", "Moguls", "Backflip"};
+String dataLabelString[5] = {"Jump","360", "540", "Moguls", "Backflip"};
 
  
 void setup() {  
@@ -57,6 +61,16 @@ void setup() {
   display.setTextSize(1);
   display.setTextColor(WHITE);
 
+  
+  Serial.print("Initializing SD card...");
+  // see if the card is present and can be initialized:
+  if (!SD.begin(chipSelect)) {
+    Serial.println("Card failed, or not present");
+    // don't do anything more:
+    while (1);
+  }
+  Serial.println("card initialized.");
+  
   // start communication with IMU 
   int status = IMU.begin();
   if (status < 0) {
@@ -76,9 +90,13 @@ bool isRecording = false;
  
 void loop() {
   for(int i=0; i<7; i++){
+    // clear the display
     display.clearDisplay();
+
+    // draw the i'th frame of the bitmap image
     display.drawBitmap(0, 1, animation[i], 51, 29, WHITE);
-    
+
+    // this displays the classification category
     display.setCursor(60,2);
     if (! digitalRead(BUTTON_A)){
       dataLabelNum++;
@@ -86,153 +104,100 @@ void loop() {
     }
     display.print(dataLabelString[dataLabelNum]);
 
-    display.setCursor(60,12);
-    if (! digitalRead(BUTTON_B)){
-      if(isRecording){
-        display.print("Start");
-        isRecording = false;  
-      }else{
-        display.print("Stop REC");
-        isRecording = true;  
-      }
-    }else{
-      if(isRecording){
-        display.print("Stop REC");
-      }else{
-        display.print("Start");
-      }
-    }
-    
 
-    // last line of displays IMU data
+    // get and display IMU data
     display.setCursor(60,22);
     IMU.readSensor();
+    
+    int xAccel = IMU.getAccelX_mss();
+    int yAccel = IMU.getAccelY_mss();
+    int zAccel = IMU.getAccelZ_mss();
 
-    char letter[5];
-    int value1, value2, value3;
+    int xGyro = IMU.getGyroX_rads();
+    int yGyro = IMU.getGyroY_rads();
+    int zGyro = IMU.getGyroZ_rads();
+
+    int xMag = IMU.getMagX_uT();
+    int yMag = IMU.getMagY_uT();
+    int zMag = IMU.getMagZ_uT();
+    
     if(! digitalRead(BUTTON_C)){
       dataIMUNum++;
       if(dataIMUNum==3) dataIMUNum = 0;
     }
-
+    // display either accel/gyro/mag data
     switch(dataIMUNum){
       case 0:
         // display the accel data
-        strcpy(letter,"A ");
-        value1 = round(IMU.getAccelX_mss());
-        value2 = round(IMU.getAccelY_mss());
-        value3 = round(IMU.getAccelZ_mss());
+        display.print("A ");
+        display.print(round(xAccel));
+        display.print(" ");
+        display.print(round(yAccel));
+        display.print(" ");
+        display.print(round(zAccel));
         break;
       
       case 1:
         // display the gyro data
-        strcpy(letter,"G ");
-        value1 = round(IMU.getGyroX_rads());
-        value2 = round(IMU.getGyroY_rads());
-        value3 = round(IMU.getGyroZ_rads());
+        display.print("G ");
+        display.print(round(xGyro));
+        display.print(" ");
+        display.print(round(yGyro));
+        display.print(" ");
+        display.print(round(zGyro));
         break;
 
       case 2:
         // display the magnetometer data
-        strcpy(letter,"M ");
-        value1 = round(IMU.getMagX_uT());
-        value2 = round(IMU.getMagY_uT());
-        value3 = round(IMU.getMagZ_uT());
+        display.print("M ");
+        display.print(round(xMag));
+        display.print(" ");
+        display.print(round(yMag));
+        display.print(" ");
+        display.print(round(zMag));
         break;
     }
-    display.print(letter);
-    display.print(value1);
-    display.print(" ");
-    display.print(value2);
-    display.print(" ");
-    display.print(value3);
+
+    // now if data needs to be saved save to the sd card files
+    String sdFileName = dataLabelString[dataLabelNum] + ".txt";
+    File dataFile = SD.open(sdFileName, FILE_WRITE);
+
+    // create the string to save
+    String data = String(xAccel) + "," + String(yAccel) + "," + String(zAccel) + "," +
+                  String(xGyro)  + "," + String(yGyro)  + "," + String(zGyro)  + "," +
+                  String(xMag)   + "," + String(yMag)   + "," + String(zMag);
     
+    display.setCursor(60,12);
+    if (! digitalRead(BUTTON_B)){
+      if(isRecording){
+        display.print("Start");
+        isRecording = false;
+        // need to signify that this specific recording is over
+        dataFile.println("end");
+      }else{
+        display.print("Stop REC");
+        isRecording = true;
+        // starting a new recording
+        dataFile.println("start");
+        dataFile.println(data);    
+      }
+    }else{
+      if(isRecording){
+        display.print("Stop REC");
+        // append data to the file
+        dataFile.println(data);  
+      }else{
+        display.print("Start");
+      }
+    }
+    dataFile.close(); // need to close the file
+
+    // finally update the display
     display.display();
     
     
     delay(100); 
   }
-
-
-  for(int i=0; i<12; i++){
-    display.clearDisplay();
-    display.drawBitmap(0, 1, animation2[i], 51, 29, WHITE);
-    
-    display.setCursor(60,2);
-    if (! digitalRead(BUTTON_A)){
-      dataLabelNum++;
-      if(dataLabelNum==5) dataLabelNum = 0; // ensure it wraps around
-    }
-    display.print(dataLabelString[dataLabelNum]);
-
-    display.setCursor(60,12);
-    if (! digitalRead(BUTTON_B)){
-      if(isRecording){
-        display.print("Start");
-        isRecording = false;  
-      }else{
-        display.print("Stop REC");
-        isRecording = true;  
-      }
-    }else{
-      if(isRecording){
-        display.print("Stop REC");
-      }else{
-        display.print("Start");
-      }
-    }
-    
-
-    // last line of displays IMU data
-    display.setCursor(60,22);
-    IMU.readSensor();
-
-    char letter[5];
-    int value1, value2, value3;
-    if(! digitalRead(BUTTON_C)){
-      dataIMUNum++;
-      if(dataIMUNum==3) dataIMUNum = 0;
-    }
-
-    switch(dataIMUNum){
-      case 0:
-        // display the accel data
-        strcpy(letter,"A ");
-        value1 = round(IMU.getAccelX_mss());
-        value2 = round(IMU.getAccelY_mss());
-        value3 = round(IMU.getAccelZ_mss());
-        break;
-      
-      case 1:
-        // display the gyro data
-        strcpy(letter,"G ");
-        value1 = round(IMU.getGyroX_rads());
-        value2 = round(IMU.getGyroY_rads());
-        value3 = round(IMU.getGyroZ_rads());
-        break;
-
-      case 2:
-        // display the magnetometer data
-        strcpy(letter,"M ");
-        value1 = round(IMU.getMagX_uT());
-        value2 = round(IMU.getMagY_uT());
-        value3 = round(IMU.getMagZ_uT());
-        break;
-    }
-    display.print(letter);
-    display.print(value1);
-    display.print(" ");
-    display.print(value2);
-    display.print(" ");
-    display.print(value3);
-    
-    display.display();
-    
-    
-    delay(100); 
-  }
-
-
-
+  
   
 }
